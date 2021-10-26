46e435b173e10050aac9e8302823b6a95280a1f2
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2021-10-26 15:59:09 -0400

    Lecture 9 Activity

--- phase-2/flatiron-bakes/src/components/Header.js
+++ phase-2/flatiron-bakes/src/components/Header.js
@@ -1,10 +1,17 @@
+import styled from 'styled-components';
+
 function Header({ bakery, slogan }) {
     return (
-        <>
+        <HeaderContainer>
             <h1>{bakery}</h1>
             <h3>{slogan}</h3>
-        </>
+        </HeaderContainer>
     );
 }
 
 export default Header;
+
+const HeaderContainer = styled.div`
+    background-color: #cdcdcd;
+    text-align: center;
+`;
