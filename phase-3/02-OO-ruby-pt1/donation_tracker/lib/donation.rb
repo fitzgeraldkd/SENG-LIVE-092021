2b5d7bdfac490c66b8aad2328ec74c8ad47233af
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2021-11-02 15:04:25 -0500

    Lecture 2

---
+++ phase-3/02-OO-ruby-pt1/donation_tracker/lib/donation.rb
@@ -0,0 +1,19 @@
+class Donation
+
+  attr_accessor :organization, :amount, :date, :completed
+
+  def initialize(organization, amount, date, completed)
+    @organization = organization
+    @amount = amount
+    @date = date
+    @completed = completed
+  end
+
+  def read_donation 
+    puts "Organization: #{@organization}"
+    puts "Amount: #{@amount}"
+    puts "Date: #{@date}"
+    puts "Completed? #{@completed}"
+  end
+
+end
